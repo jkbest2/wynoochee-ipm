8207edbaf09c6131fc2f4ab06725ecfa11ac0a7c
John K Best <John.Best@dfw.wa.gov>
2023-08-08 15:23:45 -0700

    Update 31-fit-bingham.R

--- 31-fit-bingham.R
+++ 31-fit-bingham.R
@@ -1,4 +1,6 @@
-devtools::load_all("../../src/salmonIPM")
+# devtools::load_all("../../src/salmonIPM")
+library(salmonIPM)
+library(tidyverse)
 
 bing <- read_rds("data/bing_coho_up.rds") |>
   mutate(sex = ifelse(type == "female", "female", "male"),
@@ -14,13 +16,11 @@ bing <- read_rds("data/bing_coho_up.rds") |>
 bing_harvest <- read_rds("data/bing_harvest.rds") |>
   select(year = return_year,
          ocean_survival,
-         harvest_total) |>
-  mutate(year = factor(year))
+         harvest_total)
 
 bing_df <- bing |>
   left_join(bing_harvest, by = join_by(year)) |>
   mutate(pop = factor("Bingham Creek"),
-         year = as.integer(as.character(year)),
          A = 40,
          n_W_obs = 0,
          n_H_obs = 0,
@@ -49,8 +49,11 @@ bing_fit <- salmonIPM(
   age_B = c(0, 0),
   # age_S_obs = c(TRUE, TRUE),
   # age_S_eff = c(TRUE, TRUE),
-  chains = 4, iter = 2000,
-  control = list(adapt_delta = 0.99)
+  prior = list(mu_p ~ dirichlet(c(0.1, 0.9))),
+  chains = 4, iter = 10000,
+  control = list(adapt_delta = 0.999,
+                 metric = "dense_e")
 )
 
 bing_fit
+wyn_fit
